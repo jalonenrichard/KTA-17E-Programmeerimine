dc02a7d81713c59a53fc4b7178fff725258469e2
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-10-19 13:33:31 +0300

    Lihtne kalkulaator

---
+++ KTA-17E Programmeerimine/01Prax/Exercises/04SumOfTwoNumbers/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04SumOfTwoNumbers
+{
+    class Calculator
+    {
+
+        public Calculator()
+        {
+
+        }
+
+        public double AddTwoNumbers(double numOne, double numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public double SubtractTwoNumbers(double numOne, double numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public double MultiplyTwoNumbers(double numOne, double numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public double DivideTwoNumbers(double numOne, double numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+
+    }
+}
---
+++ KTA-17E Programmeerimine/01Prax/Exercises/04SumOfTwoNumbers/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04SumOfTwoNumbers
+{
+    class Program
+    {
+        private static double numOne, numTwo;
+
+        static void Main(string[] args)
+        {
+            AskWhichFunction();
+        }
+
+        private static void AskWhichFunction()
+        {
+            Console.Write("Kas soovid liita(1), lahutada(2), korrutada(3) või jagada(4)? : ");
+            string sisend = Console.ReadLine();
+
+            Calculator calculator = new Calculator();
+
+            switch (sisend)
+            {
+                case "1":
+                    Console.WriteLine("Valisid liitmise");
+                    AskNumbers();
+                    Console.WriteLine($"\n{numOne} + {numTwo} = {calculator.AddTwoNumbers(numOne, numTwo)}");
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.WriteLine("Valisid lahutamise");
+                    AskNumbers();
+                    Console.WriteLine("\n" + numOne + " - " + numTwo + " = " + calculator.SubtractTwoNumbers(numOne, numTwo));
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.WriteLine("Valisid korrutamise");
+                    AskNumbers();
+                    Console.WriteLine("\n" + numOne + " * " + numTwo + " = " + calculator.MultiplyTwoNumbers(numOne, numTwo));
+                    Console.ReadKey();
+                    break;
+                case "4":
+                    Console.WriteLine("Valisid jagamise");
+                    AskNumbers();
+                    calculator.DivideTwoNumbers(numOne, numTwo);
+                    Console.WriteLine("\n" + numOne + " / " + numTwo + " = " + calculator.DivideTwoNumbers(numOne, numTwo));
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Vigane sisend");
+                    AskWhichFunction();
+                    break;
+            }
+        }
+
+        private static void AskNumbers()
+        {
+            Console.Write("Sisesta esimene arv: ");
+            string firstNumber = Console.ReadLine();
+            bool firstResult = Double.TryParse(firstNumber, out numOne);
+            if (!firstResult)
+            {
+                Console.WriteLine("Vigane sisend");
+                numOne = 0;
+                AskNumbers();
+            }
+
+            Console.Write("Sisesta teine arv: ");
+            string secondNumber = Console.ReadLine();
+            bool secondResult = Double.TryParse(secondNumber, out numTwo);
+            if (secondResult)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Vigane sisend");
+                numTwo = 0;
+                AskNumbers();
+            }
+        }
+
+    }
+
+
+}
