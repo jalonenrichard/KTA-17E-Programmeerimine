497853a5a8f6b6d3f1b32cad2787d92b0f5ca3ad
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-10-19 11:46:08 +0300

    User Input

---
+++ KTA-17E Programmeerimine/01Prax/Exercises/03UserInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+
+            Console.Write("Sisesta tekst: ");
+            input = Console.ReadLine();
+
+            Console.WriteLine("\nSisestasid: " + input);
+            Console.ReadKey();
+        }
+    }
+}
