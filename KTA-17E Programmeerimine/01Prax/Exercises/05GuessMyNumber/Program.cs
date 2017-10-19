9240edbc4b1b142800b240001a56af58735f3717
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-10-19 14:06:48 +0300

    GuessTheRandomNumber

---
+++ KTA-17E Programmeerimine/01Prax/Exercises/05GuessMyNumber/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05GuessMyNumber
+{
+    class Program
+    {
+        private static int userNumber, npcNumber;
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            npcNumber = random.Next(100);
+
+            Console.WriteLine("Arvuti valis suvalise numbri 1-100, proovi see ära arvata!");
+
+            GuessNumber();
+
+            while (userNumber != npcNumber)
+            {
+                if (userNumber < npcNumber)
+                {
+                    Console.WriteLine("Liiga väike number");
+                    GuessNumber();
+                }
+                else if (userNumber > npcNumber)
+                {
+                    Console.WriteLine("Liiga suur number");
+                    GuessNumber();
+                }
+            }
+
+            Console.WriteLine($"Õige! Arvuti number oli {npcNumber}");
+            Console.ReadKey();
+        }
+
+        private static void GuessNumber()
+        {
+            Console.Write("Sisesta number: ");
+            string inputString = Console.ReadLine();
+
+            bool isNumber = Int32.TryParse(inputString, out userNumber);
+            if (!isNumber || userNumber < 0 || userNumber > 100)
+            {
+                Console.WriteLine("Vigane sisend, ära tee nahka!");
+                GuessNumber();
+            }
+        }
+    }
+}
