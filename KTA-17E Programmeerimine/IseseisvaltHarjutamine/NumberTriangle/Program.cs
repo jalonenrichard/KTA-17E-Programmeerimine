b292271f1aa2748af1bdd16b7b67453ffad4e0e3
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-10-19 20:24:40 +0300

    NumberTriangle
    https://www.w3resource.com/csharp-exercises/data-types/index.php
    
    2. Write a C# Sharp program that takes a number and a width also a number, as input and then displays a triangle of that width, using that number. Go to the editor
    Test Data
    Enter a number: 6
    Enter the desired width: 6
    Expected Output :
    
    666666
    66666
    6666
    666
    66
    6

---
+++ KTA-17E Programmeerimine/IseseisvaltHarjutamine/NumberTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberTriangle
+{
+    class Program
+    {
+        private static int triangleNumber, triangleHeight;
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private void AskTriangleInputs()
+        {
+            Console.Write("Enter a number you wish to build the triangle with: ");
+            string baseNumber = Console.ReadLine();
+
+            Console.Write("How tall (amount of lines) should the triangle be: ");
+            string heightString = Console.ReadLine();
+        }
+    }
+}
---
+++ KTA-17E Programmeerimine/IseseisvaltHarjutamine/NumberTriangle/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberTriangle
+{
+    class Triangle
+    {
+        private int _triangleNumber, _triangleHeight;
+
+        public Triangle(int triangleNumber, int triangleHeight)
+        {
+            _triangleNumber = triangleNumber;
+            _triangleHeight = triangleHeight;
+        }
+
+    }
+}
