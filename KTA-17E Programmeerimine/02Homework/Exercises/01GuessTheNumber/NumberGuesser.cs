9edc57c1d539fcca9992c140131db7aea4dc9ff5
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-10-23 22:02:36 +0300

    1. kodutoo
    1. Loo programm mis kuvab kasutajale välja teksti stiilis : „Ma valin välja ühe suvalise numbri vahemikus [1 – 100]. Proovi see ära arvata“. Defineerite selle suvalise numbri esialgu ise. Küsite kasutaja käest sisendit ning kuvate vastuseks, et kas arvuti valitud number on suurem või siis väiksem.
    2. Täiendate eelnevat programmi nii, et lisaks suuremale ja väiksemale kontrollide ka võrdelisust. Kui kasutaja sisestab sama numbri mis arvuti valis (veel käsitsi defineeritud) siis ütleb, et „Arvasid numbri ära“.
    3. Täiendage eelnevat programmi nii, et arvuti valib numbri nüüd juba automaatselt.
    4. Täiendage eelnevat programmi nii, et pärast kasutaja pakkumist arvuti kuvab kas pakutud number on suurem või väiksem. Seejärel annab kasutajale uue võimaluse pakkumiseks. See käib niikaua kuni lõpuks kasutaja arvab õige numbri ära. Selleks kasutada while tsükklit ning ühte muutujat (tingimuslausena while’s, kas number on ära arvatud või mitte)
    5. Muutke eelnevat programmi nii, et kasutate while(true) tsükklit ning väljute tsüklist kasutades keyword’i ’break’

---
+++ KTA-17E Programmeerimine/02Homework/Exercises/01GuessTheNumber/NumberGuesser.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01GuessTheNumber
+{
+    class NumberGuesser
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The random number that the NPC picks
+        /// </summary>
+        public int RandomNumber { get; private set; }
+
+        /// <summary>
+        /// The number that our user inserts while trying to guess the random number
+        /// </summary>
+        public int UserNumber { get; set; }
+
+        /// <summary>
+        /// The number of guesses it took for the user to guess the random number
+        /// </summary>
+        public int NumberOfGuesses { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public NumberGuesser()
+        {
+            //Pick a random number from 1-100 when we create an instance of NumberGuesser
+            this.RandomNumber = PickTheRandomNumber();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Inform the user about the program functionality
+        /// </summary>
+        public void InformUser()
+        {
+            Console.WriteLine("I picked a random number between 1 and 100, your job is to try and guess the number.");
+        }
+
+        /// <summary>
+        /// Bug the user until they guess the random number
+        /// </summary>
+        public void AskUserInput()
+        {
+            //Keep bugging the user until they guess the random number our NPC picked
+            while (this.UserNumber != this.RandomNumber)
+            {
+                //Add +1 to the amount of tries the user has guessed
+                this.NumberOfGuesses++;
+                //Ask for the input
+                Console.Write("Your guess: ");
+                //Store the input to a local variable
+                string userInput = Console.ReadLine();
+
+                //Remove any extra whitespace if there is any
+                userInput = userInput.Trim();
+
+                //a local variable to store the parsed user input number
+                int userNumber;
+
+                //Check if the input is valid
+                //If the user input is empty
+                if (userInput == string.Empty)
+                {
+                    //... inform the user about their mistake
+                    Console.WriteLine("You entered absolutely nothing, nice try though, try again!");
+                }
+                //If the user did not enter an integer...
+                else if (!Int32.TryParse(userInput, out userNumber))
+                {
+                    //... inform the user about their mistake
+                    Console.WriteLine($"{userInput} is not a valid number, please enter an integer.");
+
+                }
+                //If the user entered a number that is 0 or less
+                else if (Int32.TryParse(userInput, out userNumber) && userNumber < 1)
+                {
+                    //... inform the user about their mistake
+                    Console.WriteLine($"{userNumber} is too small of a number, i picked a number starting from 1 and up to 100, try again.");
+                }
+                else if (Int32.TryParse(userInput, out userNumber) && userNumber > 100)
+                {
+                    //... inform the user about their mistake
+                    Console.WriteLine($"{userNumber} is too large of a number, i picked a number starting from 1 and up to 100, try again.");
+                }
+                //If the userInput is valid
+                else
+                {
+                    //Store the value to our UserNumber property
+                    this.UserNumber = int.Parse(userInput);
+
+                    //Check if the user guessed too high or too low
+                    //If the number was lower than the random number...
+                    if (this.UserNumber < this.RandomNumber)
+                    {
+                        //...inform the user that the number they picked is too low
+                        Console.WriteLine($"Too low! The number i picked is higher than {this.UserNumber}, try again!");
+                    }
+                    //If the user entered a number too high...
+                    else if (this.UserNumber > this.RandomNumber)
+                    {
+                        //Inform the user that it's too high
+                        Console.WriteLine($"Too high! The number i picked is lower than {this.UserNumber}, try again!");
+                    }
+                }
+
+
+            }
+        }
+
+        /// <summary>
+        /// Congratulate the user for guessing the number, reveal the random number and the amount of tries it took
+        /// </summary>
+        public void RevealNumber()
+        {
+            Console.WriteLine("\n*** Correct! You guessed the number! ***");
+            //Tell the user the random number picked at start to validate
+            Console.WriteLine($"The number i picked: {this.RandomNumber}");
+            //Let he user know how many tries it took them to guess the number
+            Console.WriteLine($"It took you {this.NumberOfGuesses} tries to guess my number");
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Pick a random number between 1 and 100
+        /// </summary>
+        /// <returns></returns>
+        private int PickTheRandomNumber()
+        {
+            //Create a new instance of Random object
+            var randomNumber = new Random();
+            //Pick the random number from 1 to 100
+            return randomNumber.Next(1, 101);
+        }
+
+        #endregion
+    }
+
+}
---
+++ KTA-17E Programmeerimine/02Homework/Exercises/01GuessTheNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an instance of the NumberGuesser class
+            var numberGuesser = new NumberGuesser();
+
+            //Inform the user about the functionality of the program
+            numberGuesser.InformUser();
+
+            //Bug (keep asking) the user until they guess the random number
+            numberGuesser.AskUserInput();
+
+            //Congratulate the user for guessing the number and reveal the Random Number the NPC picked
+            numberGuesser.RevealNumber();
+
+            //Wait for the user to press a key so the console window would stay open
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
