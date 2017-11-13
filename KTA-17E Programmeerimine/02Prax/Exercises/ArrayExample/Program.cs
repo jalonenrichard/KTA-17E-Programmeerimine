7533129f1cecc82b54769920eb7fff24cbb20909
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-11-13 15:00:17 +0200

    Print out different ArrayList elements

---
+++ KTA-17E Programmeerimine/02Prax/Exercises/ArrayExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("nahk");
+            arrayList.Add(5);
+            arrayList.Add(5.77);
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+
+            Console.WriteLine($"Mulle maitseb {arrayList[0]}");
+
+            arrayList.Add("Kool");
+
+            Console.WriteLine(arrayList[arrayList.Count -1]);
+            Console.ReadLine();
+        }
+    }
+}
