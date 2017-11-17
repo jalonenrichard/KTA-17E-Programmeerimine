06d3486db6faf352e9434ae709050060663f14a2
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-11-17 21:41:21 +0200

    Create the DateTime Blank Project

---
+++ KTA-17E Programmeerimine/IseseisvaltHarjutamine/Konsoolirakendused/DateTimePractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01 Current Date Time
+            // First exercise
+            /*
+             * 1. Write a program in C to print the current time. Go to the editor
+             *
+              *  Expected Output :
+              *
+              *  The Current time is : Thu Aug 03 13:38:58 2017
+             */
+
+            Console.WriteLine();
+
+            #endregion
+        }
+    }
+}
