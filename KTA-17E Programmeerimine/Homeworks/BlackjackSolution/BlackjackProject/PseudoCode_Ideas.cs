cec6e2faab88516dde89980858b0dd3a23c740f7
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2017-12-01 13:18:59 +0200

    Add pseudocode and ideas

---
+++ KTA-17E Programmeerimine/Homeworks/BlackjackSolution/BlackjackProject/PseudoCode_Ideas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackjackProject
+{
+    class PseudoCode_Ideas
+    {
+        /* 
+         * 
+         * Different classes for: 
+         * 1. Deck
+         * 2. Dealer
+         * 3. Player
+         * 4. Program (main method, communicate with the player)
+         * (maybe a separate class for Hand? containing the cards currently in hand)
+         * (maybe a gameController class also?? we'll see)
+         * 
+         * The deck class should take 2 Enums to work with:
+         * one for the numbers + jack, queen, king, ace and one for Suit ♠♥♦♣
+         * 
+         * Program logic:
+         * fill the 52? card deck using for cycles?
+         * deal the cards to dealer and player (need to take the cards out of the list so they wouldnt repeat)
+         * display the dealt hands for player and dealer(second card hidden for dealer)
+         * ask (cycle) the user what they want to do next (1 to hit, 2 to reveal/end)
+         * if hit again, deal another card from our deck (but what about house??? - hit aswell when user hits?, hmm...)
+         * if end, decide the winner and reveal the results
+         * 
+        */
+    }
+}
