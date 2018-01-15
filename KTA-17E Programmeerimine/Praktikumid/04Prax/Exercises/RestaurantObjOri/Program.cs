98b6e1935fcdd20dd9783b4a89f486cc66bfb180
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2018-01-15 10:48:42 +0200

    Create a receipt for the given tab, no discounts yet

---
+++ KTA-17E Programmeerimine/Praktikumid/04Prax/Exercises/RestaurantObjOri/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantObjOri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Restaurant restaurant = new Restaurant("Taco Kulin", "Mustamae tee 123");
+
+            Tab tab = new Tab();
+            tab.Add(4.40);
+            tab.Add(3.15);
+            tab.Add(6.60);
+
+            Receipt receipt = restaurant.GetReceipt(tab);
+
+            Console.Write(receipt);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KTA-17E Programmeerimine/Praktikumid/04Prax/Exercises/RestaurantObjOri/Receipt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantObjOri
+{
+    class Receipt
+    {
+        /// <summary>
+        /// Tab to create the receipt for
+        /// </summary>
+        private Tab _tab { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="tab">Tab to create the receipt for</param>
+        public Receipt(Tab tab)
+        {
+            this._tab = tab;
+        }
+
+        /// <summary>
+        /// Makes a string version of the Receipt
+        /// </summary>
+        /// <returns>Receipt as a string</returns>
+        public override string ToString()
+        {
+            string tempReceipt = "";
+            // Go through the list of all the items that given tab bought and add them to the string
+            foreach (double d in _tab.ItemList)
+            {
+                tempReceipt += d + " $\n";
+            }
+            // Finally print the Summary 
+            tempReceipt += "Total: " + _tab.TabSummary + " $";
+            return tempReceipt;
+        }
+    }
+
+}
