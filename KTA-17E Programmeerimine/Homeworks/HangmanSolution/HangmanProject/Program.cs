2b48ec1dab17ae2cb369ab035dea8cc3ff74689a
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2018-01-25 14:00:25 +0200

    Generate a random english word as JSON response

---
+++ KTA-17E Programmeerimine/Homeworks/HangmanSolution/HangmanProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // TESTS
+
+            // TEST the WordGenerator
+            WordGenerator wg = new WordGenerator();
+            Console.WriteLine(wg.Word);
+            Console.ReadKey();
+        }
+    }
+}
