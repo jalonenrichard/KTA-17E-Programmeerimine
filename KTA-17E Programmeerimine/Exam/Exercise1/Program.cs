18098d35e73c664f21c7b7ff77ae36847db895dd
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2018-04-09 09:59:26 +0300

    Exercise 1 finished

---
+++ KTA-17E Programmeerimine/Exam/Exercise1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Luua programm mis küsib inimeste nimesi niikaua kuni sisestatakse erimärk „-1“. 
+               Iga sisestatud nimi lisatakse massiivi mis kuvatakse kasutajale programmi lõpus välja. 
+               Kindlasti tuleb veenduda, et esimene täht oleks suur. Selle võid lahendada nii, 
+               et palud kasutajal sisestada sisendi õigel kujul või siis vaikimisi teed programmis esimese tähe alati suureks.
+             */
+            Console.WriteLine("Programm kysib teilt nimesid kuni sisestate -1 ning hiljem kuvab koik sisestatud nimed.");
+            Console.WriteLine();
+
+            List<string> nameList = new List<string>(AskNames());
+            if (nameList.Any())
+            {
+                Console.WriteLine();
+                Console.WriteLine("Sisestatud nimed:");
+                foreach (string name in nameList)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Vajuta ykskoik millist nuppu, et v2ljuda programmist.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Ask the user to input names until -1 is inserted and return a list of names
+        /// </summary>
+        /// <returns>a list of names the user typed in</returns>
+        static List<string> AskNames()
+        {
+            List<string> nameList = new List<string>();
+            string userInput = "";
+            while (userInput != "-1")
+            {
+                Console.Write("Sisesta nimi: ");
+                userInput = Console.ReadLine().Trim();
+                if (userInput != "-1" && userInput != "")
+                {
+                    nameList.Add(userInput);
+                }
+            }
+            return nameList;
+        }
+    }
+}
