3496beda6d20d94983679b81766d715a95796e3c
jalonenrichard <32843817+jalonenrichard@users.noreply.github.com>
2018-04-09 09:23:39 +0300

    Initial commit

---
+++ KTA-17E Programmeerimine/Exam/ExamProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
